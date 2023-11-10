222fae1c39a336e36766fb920c3eb0b44f4134f5
rtrevill <139879271+rtrevill@users.noreply.github.com>
2023-11-10 15:11:47 +1100

    created trial mysql statements to test functionality

--- index.js
+++ index.js
@@ -8,17 +8,23 @@ function questions(){
             {
                 type: 'list',
                 name: 'options',
-                message: 'Please choose one of the following',
+                message: 'what would you like to do? (Use arrow keys)',
                 choices: [
-                    "option1",
-                    "option2",
-                    "option3"
+                    "View All Employees",
+                    "Add Employee",
+                    "Update Employee Role",
+                    "View All Roles",
+                    "Add Role",
+                    "View All Departments",
+                    "Add Department",
+                    "Quit"
                 ]
             }
 
         ])
         .then((answer) => {
             console.log(answer)
+            viewRequests(answer)
         })
         .catch((err) => {
             console.log(err)
@@ -27,3 +33,56 @@ function questions(){
 };
 
 questions();
+
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // TODO: Add MySQL password here
+      password: '{rgh>fl@YOkUPL3O>+SN',
+      database: 'employees_db'
+    },
+    console.log(`Connected to the employees_db database.`)
+  );
+  
+
+
+function viewRequests(request){
+    if (request = "View All Employees"){
+        db.query(`SELECT employee.id, first_name, last_name, title, name AS department, salary, manager_id FROM employee JOIN role ON employee.role_id = role.id JOIN department ON department.id = role.department_id`, (err, result) => {
+            if (err){
+                console.log(err);
+                return;
+            }
+            console.table(result);
+            questions();
+            return;
+        });
+    }
+    if (request = "View All Departments"){
+        db.query(`SELECT * FROM department ORDER BY name ASC`, (err, result) => {
+            if (err){
+                console.log(err);
+                return;
+            }
+            console.table(result);
+            questions();
+            return;
+        });
+    }
+    if (request = "View All Roles"){
+        db.query(`SELECT role.id, title, name AS department, salary FROM role JOIN department ON role.department_id = department.id`, (err, result) => {
+            if (err){
+                console.log(err);
+                return;
+            }
+            console.table(result);
+            questions();
+            return;
+        });
+    }
+    else {
+        console.log('No return')
+    }
+};
