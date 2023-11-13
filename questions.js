282c03d32f493db433e37daf36179cf6019d2b83
rtrevill <139879271+rtrevill@users.noreply.github.com>
2023-11-13 11:44:09 +1100

    moved all inquirer questions to Questions.js file

--- questions.js
+++ questions.js
@@ -1,4 +1,3 @@
-const index = require('./index');
 
 class Questions{
 
@@ -23,9 +22,84 @@ listQuestions = function(){
     return mainQList;
 };
 
+addEmployQuestions = function(roleArray, manArray){
+    const employQuest = [
+        {
+            type: 'input',
+            name: 'firstName',
+            message: "What is the employee's first name?"
+        },
+        {
+            type: 'input',
+            name: 'lastName',
+            message: 'What is the employees last name?'
+        },
+        {
+            type: 'list',
+            name: 'employRole',
+            message: "What is the employee's role?",
+            choices: roleArray
+        },
+        {
+            type: 'list',
+            name: 'employMan',
+            message: "Who is the employee's manager?",
+            choices: manArray
+        }
+    ]
+    return employQuest;
+};
 
+addRollQuestions = function(departmentArray){
+    const roleQuest = [
+        {
+            type: 'input',
+            name: 'newRole',
+            message: 'What is the name of the role?'
+        },
+        {
+            type: 'input',
+            name: 'roleSalary',
+            message: 'What is the salary of the role?'
+        },
+        {
+            type: 'list',
+            name: 'roleDept',
+            message: 'Which department does the role belong to?',
+            choices: departmentArray
+        }
+    ]
+    return roleQuest;
+};
 
+addDeptQuestions = function(){
+    const deptQuest = [
+        {
+            type: 'input',
+            name: 'newDepart',
+            message: 'What is the name of the department?'
+        }
+    ]
+    return deptQuest;
+};
 
+updateRoleQuestions = function(nameArray, roleArray){
+    const upRoleQuest = [
+        {
+            type: 'list',
+            name: 'fullName',
+            message: "Which employee's role do you want to update?",
+            choices: nameArray
+        },
+        {
+            type: 'list',
+            name: 'newRole',
+            message: "Which role do you want to assign to the selected employee?",
+            choices: roleArray
+        }
+    ]
+    return upRoleQuest;
+};
 
 };
 
